e324e940fa7d65674bc6be17b4c5d36d051b5247
lijinhe <lijinhe@aisino.com>
2020-02-10 15:54:23 +0800

    add 我的页面顶部导航栏

--- js/page/MyPage.js
+++ js/page/MyPage.js
@@ -2,16 +2,59 @@ import React, {Component} from 'react';
 import {
     View,
     Text,
-    StyleSheet, Button,
+    StyleSheet,
+    Button,
+    TouchableOpacity
 } from 'react-native';
 import actions from '../action';
 import {connect} from 'react-redux';
+import NavigationBar from '../common/NavigationBar';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+const THEME_COLOR='#678';
 class MyPage extends Component {
+    getRightButton() {
+        return <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+                onPress={() => {}}
+            >
+                <View style={{padding: 5, marginRight: 8}}>
+                    <Feather
+                        name={'search'}
+                        size={24}
+                        style={{color: 'white'}}
+                    />
+                </View>
+            </TouchableOpacity>
+            </View>
+    }
+
+    getLeftButton(callback) {
+        return <TouchableOpacity style={{padding: 8, paddingLeft: 12}} onPress={callback}>
+            <Ionicons
+                name={'ios-arrow-back'}
+                size={24}
+                style={{color: 'white'}}
+            />
+        </TouchableOpacity>
+    }
+
     render() {
-        const {navigation} = this.props;
+        let statusBar = {
+            backgroundColor: THEME_COLOR,
+            barStyle: 'light-content'
+        };
+        let navigationBar = <NavigationBar
+            title={'我的'}
+            statusBar={statusBar}
+            style={{backgroundColor: THEME_COLOR}}
+            rightButton={this.getRightButton()}
+            leftButton={this.getLeftButton()}
+        />;
         return (
             <View style={styles.container}>
+                {navigationBar}
                 <Text style={styles.welcome}>MyPage</Text>
                 <Button
                     title={'修改主题'}
@@ -25,8 +68,6 @@ class MyPage extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF'
     },
     welcome: {
